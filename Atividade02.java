9463f2d281a591fa48f5f51367e84e5115ea744e
marioreisjunior <124594785+marioreisjunior@users.noreply.github.com>
2024-03-15 21:44:10 -0300

    Create Atividade02.java

---
+++ Atividade02.java
@@ -0,0 +1,18 @@
+//corrigindo atividade 02 da aula 02
+
+package teste;
+
+public class Atividade3 {
+	
+	public static void main (String[] args ) {
+		String nome = "Mario";
+		//System.out.println("nome " + nome);
+		
+		int idade = 18;
+	    int batimento = 220 - idade;
+	    
+	   // System.out.println("idade "+ idade);
+	   // System.out.println("batimento "+ batimento);
+	    System.out.println("o nome da pessoa é "+nome+" tem "+idade+" anos e o batimento está "+batimento);
+	}
+}
