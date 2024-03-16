b2950aa07a03a5cbc3fb7ca2a64cf2fb34a93f06
marioreisjunior <124594785+marioreisjunior@users.noreply.github.com>
2024-03-15 21:43:36 -0300

    Create Atividade01.java

---
+++ Atividade01.java
@@ -0,0 +1,17 @@
+//corrigindo atividade 01 da aula 02
+
+package teste;
+
+public class Atividade2 {
+public static void main (String[] args ) {
+	
+	var sexo = "M";
+	String nome = "Mario Reis";
+	int idade = 18;
+	
+	System.out.println("sexo " + sexo);
+	System.out.println("nome " + nome);
+	System.out.println("idade " +idade);
+	
+}
+}
