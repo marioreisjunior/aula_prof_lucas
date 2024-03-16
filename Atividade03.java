156f9e73eb6a8215c98774a4dc07011acb9fa32e
marioreisjunior <124594785+marioreisjunior@users.noreply.github.com>
2024-03-15 21:44:44 -0300

    Create Atividade03.java

---
+++ Atividade03.java
@@ -0,0 +1,14 @@
+//corrigindo atividade 03 da aula 02
+
+package teste;
+
+public class Atividade4 {
+	public static void main (String[] args ) {
+		String nome = "Mario";
+		int cpf = 1305792190;
+		int cep = 81938;
+		String cidadeestado = "Curitiba PR";
+		System.out.println("A pessoa com o nome "+nome+" registrada no cpf "+cpf+" é residente do cep "+cep+" em "+cidadeestado );
+		
+	}
+}
